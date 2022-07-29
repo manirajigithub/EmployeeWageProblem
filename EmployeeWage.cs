5d9681ec101f7e8cf07e5b281c07b517a16108e8
mani1 <mani1@NANI143>
2022-07-29 12:00:05 +0530

    [Raji]Added EmployeeWage uc1

---
+++ EmployeeWage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public static class EmployeeWage
+    {
+        public static void CheckEmployeeAttendace()
+        {
+            int FullTime = 1;
+
+            Random random = new Random();
+            int empcheck = random.Next();
+            if (empcheck == FullTime)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+
+            }
+        }
+    }
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace EmployeeWage
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWage Computation");
+            EmployeeWage.CheckEmployeeAttendace();
+        }
+    }
+}
