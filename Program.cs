fc274e5355714bbba169510d1a5ef6bc1e0b6495
mani1 <mani1@NANI143>
2022-07-30 06:03:57 +0530

    [Raji]added Employee Wage2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace EmployeeWageProblem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program .");
+            //CheckEmployee.CheckEmployeePresentOrNot();
+            DailyWage.CalculateDailyWage();
+        }
+    }
+}
